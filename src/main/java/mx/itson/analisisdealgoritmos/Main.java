c381c4bf8ecfe276ac65ebfe739c47cb45c9ad9e
Andrey <andreyenrique132@gmail.com>
2026-03-08 18:44:47 -0700

    Segundo commit, se hizo el algoritmo de ordenamiento por seleccion y se implemento un pequeño menu para elegir con que algoritmo de ordenamiento se quiere usar

--- src/main/java/mx/itson/analisisdealgoritmos/Main.java
+++ src/main/java/mx/itson/analisisdealgoritmos/Main.java
@@ -3,26 +3,49 @@ package mx.itson.analisisdealgoritmos;
 
 // @author Andrey
 
+import java.util.Scanner;
+
+
 public class Main 
 {
     
     
     public static void main(String[] args) {
         
-        int[] arreglo = {10, 4, 24, 3, 8, 12};
-        
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arregloOriginal = {10, 4, 24, 3, 8, 12};
+
         System.out.println("Arreglo original:");
-        
-        for (int num : arreglo) {
+
+        for (int num : arregloOriginal) {
             System.out.print(num + ", ");
         }
-        
+
         System.out.println("\n");
-        
-        Ordenamientos.burbuja(arreglo);
-        
+        System.out.println("Seleccione el algoritmo de ordenamiento:");
+        System.out.println("1.- Burbuja");
+        System.out.println("2.- Seleccion");
+
+        int opcion = scanner.nextInt();
+
+        // clonamos el arreglo para no modificar el original
+        int[] arreglo = arregloOriginal.clone();
+
+        if (opcion == 1) {
+            System.out.println("\nUsando ordenamiento Burbuja\n");
+            Ordenamientos.burbuja(arreglo);
+        }
+        else if (opcion == 2) {
+            System.out.println("\nUsando ordenamiento por Seleccion\n");
+            Ordenamientos.seleccion(arreglo);
+        } 
+        else {
+            System.out.println("Opcion no valida");
+            return;
+        }
+
         System.out.println("Arreglo ordenado:");
-        
         for (int num : arreglo) {
             System.out.print(num + ", ");
         }
--- src/main/java/mx/itson/analisisdealgoritmos/Ordenamientos.java
+++ src/main/java/mx/itson/analisisdealgoritmos/Ordenamientos.java
@@ -10,20 +10,94 @@ public class Ordenamientos
     public static void burbuja(int[] arreglo) {
 
         int n = arreglo.length; // tamaño del arreglo
+        // 1 operacion : asignacion
 
         for (int i = 0; i < n - 1; i++) {          // ciclo exterior
+        // 1 asignacion inicial
+        // (n-1) comparaciones
+        // (n-1) incrementos
+        // El ciclo exterior se ejecuta (n-1) veces
+        
             for (int j = 0; j < n - i - 1; j++) {  // ciclo interior
+            // 1 asignacion inicial
+            // comparaciones dependientes de n
+            // incrementos dependientes de n
+            // Total de comparaciones del algoritmo:
+            // (n-1) + (n-2) + ... + 1 = n(n-1)/2
 
                 if (arreglo[j] > arreglo[j + 1]) { // comparacion
+                // 1 comparacion entre elementos
 
                     int aux = arreglo[j];          // intercambio
+                    // 1 asignacion
                     arreglo[j] = arreglo[j + 1];
+                    // 1 asignacion
                     arreglo[j + 1] = aux;
+                    // 1 asignacion
                 }
             }
         }
+        /*
+        Total de comparaciones:
+        n(n-1)/2
 
-        // Complejidad aproximada: O(n^2)
+        Número maximo de intercambios:
+        n(n-1)/2
+
+        Expresion aproximada del algoritmo:
+        T(n) = an^2 + bn + c
+
+        Complejidad temporal:
+        O(n^2)
+        */
+    }
+    
+    
+    public static void seleccion(int[] arreglo) {
+
+        int n = arreglo. length;
+        // 1 operacion : asignacion
+        
+        for (int i = 0; i < n - 1; i++){
+        // 1 asignacion inicial
+        // (n-1) comparaciones
+        // (n-1) incrementos
+        // El ciclo exterior se ejecuta (n-1) veces
+        
+            int indiceMinimo = i;
+            // 1 asignacion
+            
+            for (int j = i + 1; j < n; j++){
+            // 1 asignacion inicial
+            // comparaciones dependientes de n
+            // incrementos dependientes de n
+                
+                if (arreglo[j] < arreglo[indiceMinimo]){
+                // comparacion entre elementos
+                    indiceMinimo = j;
+                    // 1 asinacion
+                }
+            } 
+            int aux = arreglo[indiceMinimo];
+            // 1 asignacion
+            arreglo[indiceMinimo] = arreglo[i];
+            // 1 asignacion
+            arreglo[i] = aux;
+            // 1 asignacion
+        }
+        /*
+        Total de comparaciones:
+        n(n-1)/2
+
+        Numero maximo de intercambios:
+        n-1
+
+        Expresion aproximada del algoritmo:
+        T(n) = an^2 + bn + c
+
+        Complejidad temporal:
+        O(n^2)
+        */        
     }
     
     
